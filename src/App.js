ede71ef65c5650398c7441a3e50ae2974c90c9fe
kim jiyeong <youngdev96@naver.com>
2024-07-14 18:15:09 +0900

    feat: 게임 시작 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  constructor() {
+    this.answer = null;
+  }
+
+  //📌 게임 시작 기능
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.answer = this.generateAnswer();
+    await this.runGame();
+  }
 }
 
+const app = new App();
+app.play();
 export default App;
